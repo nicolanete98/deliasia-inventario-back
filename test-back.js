bbd665b8db7447e17de072e3a647c0a48764884e
Nicolas Lanete <nlanete@arkho.io>
2023-03-25 11:33:53 -0500

    make it better

--- test-back.js
+++ test-back.js
@@ -189,5 +189,5 @@ app.post('/post-movimiento', (req, res) => {
 
   
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${process.env.PORT||port}`)
   })  
